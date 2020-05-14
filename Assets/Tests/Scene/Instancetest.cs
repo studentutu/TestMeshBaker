7276ee3b9ba03869ba7f027483857b26a1bd0d98
Roman Fedorov <fedorov@plavalaguna.ru>
2020-05-14 21:48:51 +0300

    added instance Test - GLES2 and GLES3.0 does not support it

---
+++ Assets/Tests/Scene/Instancetest.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Instancetest : MonoBehaviour
+{
+    [SerializeField] private Mesh meshToUse = null;
+    [SerializeField] private Material material = null;
+    [SerializeField] private Transform[] transfromsToMatrices = new Transform[2];
+    private Matrix4x4[] workingMatrixes = null;
+    private bool UseInstancing;
+
+    // Update is called once per frame
+
+    private void Awake()
+    {
+        if (SystemInfo.graphicsDeviceType == UnityEngine.Rendering.GraphicsDeviceType.OpenGLES2)
+        {
+            UseInstancing = false;
+        }
+    }
+    void Update()
+    {
+        if (workingMatrixes == null)
+        {
+            workingMatrixes = new Matrix4x4[transfromsToMatrices.Length];
+        }
+        for (int i = 0; i < transfromsToMatrices.Length; i++)
+        {
+            workingMatrixes[i] = Matrix4x4.TRS(transfromsToMatrices[i].position, transfromsToMatrices[i].rotation, transfromsToMatrices[i].lossyScale);
+        }
+        if (UseInstancing)
+        {
+            Graphics.DrawMeshInstanced(meshToUse,
+                                        0,
+                                        material,
+                                        workingMatrixes,
+                                        transfromsToMatrices.Length);
+        }
+        else
+        {
+            for (int i = 0; i < transfromsToMatrices.Length; i++)
+            {
+                Graphics.DrawMesh(meshToUse, workingMatrixes[i],
+                                           material,
+                                           0,
+                                           null,
+                                           0);
+            }
+        }
+    }
+}
